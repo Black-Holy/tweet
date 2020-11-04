c0a4966180bbce06c962e2c164fa4872126715dc
EC2 Default User <ec2-user@ip-172-31-28-248.us-east-2.compute.internal>
2020-11-04 05:29:36 +0000

    プロフィール編集画面微調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :image, :description, :birthday, :password, :password_confirmation)
   end
   
   #管理者権限
